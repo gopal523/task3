5d539c835bad6d5147ca92a73ab5b394f612de74
Gopal <gopalkumaryahoo@gmail.com>
2019-03-26 18:08:10 +0530

    firstcommit

---
+++ src/Sam.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Sam {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of test cases u want");
+		int c=sc.nextInt();
+		int arr[]=new int[10];
+		int k=0;
+		for(int i=0;i<c;i++)
+		{
+			System.out.println("test case"+ " "+i);
+			System.out.println("Ener the values of switch");
+			
+			for(int j=0;j<10;j++)
+			{
+				arr[j] = sc.nextInt();
+			}
+			System.out.println("Enter the integer k");
+			k = sc.nextInt();
+		}
+		for(int i=0;i<c;i++)
+		{
+			System.out.println("case"+ +i);
+			for(int j=0;j<10;j++)
+			{
+				System.out.print(arr[j]);
+		}
+			System.out.println();
+			System.out.println(k);
+			System.out.println();
+		}
+		
+	}
+}
